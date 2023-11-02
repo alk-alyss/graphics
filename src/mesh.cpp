2777f9b000c54bf419615275c916a4956f71b0aa
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-11-02 18:29:41 +0200

    transition mesh to use indexed face set

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -2,85 +2,31 @@
 
 #include <glm/gtc/matrix_transform.hpp>
 
-// Mesh::Mesh() {
-//     vertices = {
-//         1.0f, 1.0f, 1.0f,
-//         -1.0f, 1.0f, 1.0f,
-//         1.0f, -1.0f, 1.0f,
-//         -1.0f, -1.0f, 1.0f,
-//         1.0f, 1.0f, -1.0f,
-//         -1.0f, 1.0f, -1.0f,
-//         1.0f, -1.0f, -1.0f,
-//         -1.0f, -1.0f, -1.0f,
-//     };
-
-//     faces = {
-//         5, 7, 6,
-//         5, 6, 4,
-//         4, 6, 2,
-//         4, 2, 0,
-//         1, 5, 4,
-//         1, 4, 0,
-//         7, 3, 2,
-//         7, 2, 6,
-//         1, 3, 7,
-//         1, 7, 5,
-//         0, 2, 3,
-//         0, 3, 1
-//     };
-
-//     colors = {
-//         0.583f,  0.771f,  0.014f,
-//         0.609f,  0.115f,  0.436f,
-//         0.327f,  0.483f,  0.844f,
-//         0.822f,  0.569f,  0.201f,
-//         0.435f,  0.602f,  0.223f,
-//         0.310f,  0.747f,  0.185f,
-//         0.597f,  0.770f,  0.761f,
-//         0.559f,  0.436f,  0.730f,
-//     };
-
-//     modelMatrix = glm::translate(glm::mat4(), glm::vec3(-0.5f, -0.5f, -0.5f));
-// }
-
 Mesh::Mesh() {
     vertices = {
-        -1.0f,-1.0f,-1.0f, // triangle 1 : begin
-        -1.0f,-1.0f, 1.0f,
-        -1.0f, 1.0f, 1.0f, // triangle 1 : end
-        1.0f, 1.0f,-1.0f, // triangle 2 : begin
-        -1.0f,-1.0f,-1.0f,
-        -1.0f, 1.0f,-1.0f, // triangle 2 : end
-        1.0f,-1.0f, 1.0f,
-        -1.0f,-1.0f,-1.0f,
-        1.0f,-1.0f,-1.0f,
-        1.0f, 1.0f,-1.0f,
-        1.0f,-1.0f,-1.0f,
-        -1.0f,-1.0f,-1.0f,
-        -1.0f,-1.0f,-1.0f,
-        -1.0f, 1.0f, 1.0f,
-        -1.0f, 1.0f,-1.0f,
-        1.0f,-1.0f, 1.0f,
-        -1.0f,-1.0f, 1.0f,
-        -1.0f,-1.0f,-1.0f,
-        -1.0f, 1.0f, 1.0f,
-        -1.0f,-1.0f, 1.0f,
-        1.0f,-1.0f, 1.0f,
-        1.0f, 1.0f, 1.0f,
-        1.0f,-1.0f,-1.0f,
-        1.0f, 1.0f,-1.0f,
-        1.0f,-1.0f,-1.0f,
-        1.0f, 1.0f, 1.0f,
-        1.0f,-1.0f, 1.0f,
-        1.0f, 1.0f, 1.0f,
-        1.0f, 1.0f,-1.0f,
-        -1.0f, 1.0f,-1.0f,
-        1.0f, 1.0f, 1.0f,
-        -1.0f, 1.0f,-1.0f,
-        -1.0f, 1.0f, 1.0f,
-        1.0f, 1.0f, 1.0f,
-        -1.0f, 1.0f, 1.0f,
-        1.0f,-1.0f, 1.0f
+        1.0f,1.0f,1.0f,		// 0
+        1.0f,1.0f,-1.0f,	// 1
+        1.0f,-1.0f,1.0f,	// 2
+        1.0f,-1.0f,-1.0f,	// 3
+        -1.0f,1.0f,1.0f,	// 4
+        -1.0f,1.0f,-1.0f,	// 5
+        -1.0f,-1.0f,1.0f,	// 6
+        -1.0f,-1.0f,-1.0f,  // 7
+    };
+
+    faces = {
+        4,2,0,	// Front Face
+        4,6,2,
+        0,3,1,	// Right Face
+        0,2,3,
+        1,7,5,	// Back Face
+        1,3,7,
+        5,6,4,	// Left Face
+        5,7,6,
+        4,1,5,	// Top Face
+        4,0,1,
+        6,3,2,	// Bottom Face
+        6,7,3
     };
 
     colors = {
@@ -92,34 +38,6 @@ Mesh::Mesh() {
         0.310f,  0.747f,  0.185f,
         0.597f,  0.770f,  0.761f,
         0.559f,  0.436f,  0.730f,
-        0.359f,  0.583f,  0.152f,
-        0.483f,  0.596f,  0.789f,
-        0.559f,  0.861f,  0.639f,
-        0.195f,  0.548f,  0.859f,
-        0.014f,  0.184f,  0.576f,
-        0.771f,  0.328f,  0.970f,
-        0.406f,  0.615f,  0.116f,
-        0.676f,  0.977f,  0.133f,
-        0.971f,  0.572f,  0.833f,
-        0.140f,  0.616f,  0.489f,
-        0.997f,  0.513f,  0.064f,
-        0.945f,  0.719f,  0.592f,
-        0.543f,  0.021f,  0.978f,
-        0.279f,  0.317f,  0.505f,
-        0.167f,  0.620f,  0.077f,
-        0.347f,  0.857f,  0.137f,
-        0.055f,  0.953f,  0.042f,
-        0.714f,  0.505f,  0.345f,
-        0.783f,  0.290f,  0.734f,
-        0.722f,  0.645f,  0.174f,
-        0.302f,  0.455f,  0.848f,
-        0.225f,  0.587f,  0.040f,
-        0.517f,  0.713f,  0.338f,
-        0.053f,  0.959f,  0.120f,
-        0.393f,  0.621f,  0.362f,
-        0.673f,  0.211f,  0.457f,
-        0.820f,  0.883f,  0.371f,
-        0.982f,  0.099f,  0.879f
     };
 
     loadVram();
@@ -148,9 +66,9 @@ void Mesh::loadVram() {
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
     glEnableVertexAttribArray(1);
 
-    // glGenBuffers(1, &EBO);
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, faces.size(), faces.data(), GL_STATIC_DRAW);
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, faces.size() * sizeof(faces[0]), faces.data(), GL_STATIC_DRAW);
 
     glBindVertexArray(0);
 }
@@ -173,6 +91,6 @@ void Mesh::unloadVram() {
 
 void Mesh::draw() {
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+    glDrawElements(GL_TRIANGLES, faces.size(), GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 }
