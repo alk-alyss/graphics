18abe6950701d5d2f600b8d3c0a0aba152b1d0ae
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-12-18 20:28:23 +0200

    update mesh

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,50 +1,133 @@
 #include "mesh.hpp"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <stdexcept>
+#include <cstring>
 
-Mesh::Mesh() {
-    vertices = {
-        1.0f,1.0f,1.0f,		// 0
-        1.0f,1.0f,-1.0f,	// 1
-        1.0f,-1.0f,1.0f,	// 2
-        1.0f,-1.0f,-1.0f,	// 3
-        -1.0f,1.0f,1.0f,	// 4
-        -1.0f,1.0f,-1.0f,	// 5
-        -1.0f,-1.0f,1.0f,	// 6
-        -1.0f,-1.0f,-1.0f,  // 7
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+
+using namespace std;
+using namespace glm;
+
+void loadOBJWithTiny(
+    const string& path,
+    vector<vec3>& vertices,
+    vector<vec2>& uvs,
+    vector<vec3>& normals,
+    vector<unsigned int>& indices) {
+    tinyobj::attrib_t attrib;
+    vector<tinyobj::shape_t> shapes;
+    vector<tinyobj::material_t> materials;
+
+    string err, warn;
+    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, path.c_str())) {
+        throw runtime_error(err);
+    }
+
+    for (const auto& shape : shapes) {
+        for (const auto& index : shape.mesh.indices) {
+            vec3 vertex = {
+                attrib.vertices[3 * index.vertex_index + 0],
+                attrib.vertices[3 * index.vertex_index + 1],
+                attrib.vertices[3 * index.vertex_index + 2]};
+            if (attrib.texcoords.size() != 0) {
+                vec2 uv = {
+                    attrib.texcoords[2 * index.texcoord_index + 0],
+                    1 - attrib.texcoords[2 * index.texcoord_index + 1]};
+                uvs.push_back(uv);
+            }
+            if (attrib.normals.size() != 0) {
+                vec3 normal = {
+                    attrib.normals[3 * index.normal_index + 0],
+                    attrib.normals[3 * index.normal_index + 1],
+                    attrib.normals[3 * index.normal_index + 2]};
+                normals.push_back(normal);
+            }
+
+            vertices.push_back(vertex);
+            indices.push_back(indices.size());
+        }
+    }
+
+    // TODO .mtl loader
+}
+
+struct PackedVertex {
+    glm::vec3 position;
+    glm::vec2 uv;
+    glm::vec3 normal;
+    bool operator<(const PackedVertex that) const {
+        return memcmp((void*) this, (void*) &that, sizeof(PackedVertex)) > 0;
     };
+};
 
-    faces = {
-        4,2,0,	// Front Face
-        4,6,2,
-        0,3,1,	// Right Face
-        0,2,3,
-        1,7,5,	// Back Face
-        1,3,7,
-        5,6,4,	// Left Face
-        5,7,6,
-        4,1,5,	// Top Face
-        4,0,1,
-        6,3,2,	// Bottom Face
-        6,7,3
-    };
+bool getSimilarVertexIndex(
+    PackedVertex& packed,
+    map<PackedVertex, unsigned int>& vertexToOutIndex,
+    unsigned int& result) {
+    map<PackedVertex, unsigned int>::iterator it = vertexToOutIndex.find(packed);
+    if (it == vertexToOutIndex.end()) {
+        return false;
+    } else {
+        result = it->second;
+        return true;
+    }
+}
 
-    colors = {
-        0.583f,  0.771f,  0.014f,
-        0.609f,  0.115f,  0.436f,
-        0.327f,  0.483f,  0.844f,
-        0.822f,  0.569f,  0.201f,
-        0.435f,  0.602f,  0.223f,
-        0.310f,  0.747f,  0.185f,
-        0.597f,  0.770f,  0.761f,
-        0.559f,  0.436f,  0.730f,
-    };
+void indexVBO(
+    const vector<vec3>& in_vertices,
+    const vector<vec2>& in_uvs,
+    const vector<vec3>& in_normals,
+    vector<unsigned int>& out_indices,
+    vector<vec3>& out_vertices,
+    vector<vec2>& out_uvs,
+    vector<vec3>& out_normals) {
+    map<PackedVertex, unsigned int> vertexToOutIndex;
 
+    // For each input vertex
+    for (int i = 0; i < static_cast<int>(in_vertices.size()); i++) {
+        vec3 vertices = in_vertices[i];
+        vec2 uvs;
+        vec3 normals;
+        if (in_uvs.size() != 0) uvs = in_uvs[i];
+        if (in_normals.size() != 0) normals = in_normals[i];
+        PackedVertex packed = {vertices, uvs, normals};
+
+        // Try to find a similar vertex in out_XXXX
+        unsigned int index;
+        bool found = getSimilarVertexIndex(packed, vertexToOutIndex, index);
+
+        if (found) { // A similar vertex is already in the VBO, use it instead !
+            out_indices.push_back(index);
+        } else { // If not, it needs to be added in the output data.
+            out_vertices.push_back(vertices);
+            if (in_uvs.size() != 0) out_uvs.push_back(uvs);
+            if (in_normals.size() != 0) out_normals.push_back(normals);
+            unsigned int newindex = (unsigned int) out_vertices.size() - 1;
+            out_indices.push_back(newindex);
+            vertexToOutIndex[packed] = newindex;
+        }
+    }
+}
+
+Mesh::Mesh(
+    const vector<vec3>& vertices,
+    const vector<vec2>& uvs,
+    const vector<vec3>& normals
+) : vertices(vertices), uvs(uvs), normals(normals) {
+    indexVBO(vertices, uvs, normals, indices, indexedVertices, indexedUVS, indexedNormals);
     loadVram();
 }
 
-Mesh::Mesh(std::string filename) {
+Mesh::Mesh(std::string path) {
+    if (path.substr(path.size() - 3, 3) == "obj") {
+        loadOBJWithTiny(path.c_str(), vertices, uvs, normals, VEC_UINT_DEFAUTL_VALUE);
+    } else {
+        throw runtime_error("File format not supported: " + path);
+    }
 
+    indexVBO(vertices, uvs, normals, indices, indexedVertices, indexedUVS, indexedNormals);
     loadVram();
 }
 
@@ -52,23 +135,31 @@ void Mesh::loadVram() {
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
-    glGenBuffers(1, &vertexVBO);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexVBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices[0]), vertices.data(), GL_STATIC_DRAW);
-
+    glGenBuffers(1, &verticesVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, verticesVBO);
+    glBufferData(GL_ARRAY_BUFFER, indexedVertices.size() * sizeof(indexedVertices[0]), indexedVertices.data(), GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
     glEnableVertexAttribArray(0);
 
-    glGenBuffers(1, &colorVBO);
-    glBindBuffer(GL_ARRAY_BUFFER, colorVBO);
-    glBufferData(GL_ARRAY_BUFFER, colors.size() * sizeof(colors[0]), colors.data(), GL_STATIC_DRAW);
+    if (indexedNormals.size() != 0) {
+        glGenBuffers(1, &normalsVBO);
+        glBindBuffer(GL_ARRAY_BUFFER, normalsVBO);
+        glBufferData(GL_ARRAY_BUFFER, indexedNormals.size() * sizeof(indexedNormals[0]), indexedNormals.data(), GL_STATIC_DRAW);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+        glEnableVertexAttribArray(1);
+    }
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-    glEnableVertexAttribArray(1);
+    if (indexedUVS.size() != 0) {
+        glGenBuffers(1, &uvsVBO);
+        glBindBuffer(GL_ARRAY_BUFFER, uvsVBO);
+        glBufferData(GL_ARRAY_BUFFER, indexedUVS.size() * sizeof(indexedUVS[0]), indexedUVS.data(), GL_STATIC_DRAW);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+        glEnableVertexAttribArray(2);
+    }
 
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, faces.size() * sizeof(faces[0]), faces.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(indices[0]), indices.data(), GL_STATIC_DRAW);
 
     glBindVertexArray(0);
 }
@@ -83,14 +174,15 @@ glm::mat4 Mesh::modelMatrix() {
 }
 
 void Mesh::unloadVram() {
-    glDeleteBuffers(1, &vertexVBO);
-    glDeleteBuffers(1, &colorVBO);
+    glDeleteBuffers(1, &verticesVBO);
+    glDeleteBuffers(1, &normalsVBO);
+    glDeleteBuffers(1, &uvsVBO);
     glDeleteBuffers(1, &EBO);
     glDeleteVertexArrays(1, &VAO);
 }
 
 void Mesh::draw() {
     glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, faces.size(), GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 }
--- src/mesh.hpp
+++ src/mesh.hpp
@@ -3,29 +3,50 @@
 
 #include <vector>
 #include <string>
-
+#include <map>
 #include <GL/glew.h>
-
 #include <glm/glm.hpp>
 
-#include <shader.hpp>
+static std::vector<unsigned int> VEC_UINT_DEFAUTL_VALUE{};
+static std::vector<glm::vec3> VEC_VEC3_DEFAUTL_VALUE{};
+static std::vector<glm::vec2> VEC_VEC2_DEFAUTL_VALUE{};
+static std::map<std::string, GLuint> MAP_STRING_GLUINT_DEFAULT_VALUE{};
+
+void loadOBJWithTiny(
+    const std::string& path,
+    std::vector<glm::vec3>& vertices,
+    std::vector<glm::vec2>& uvs,
+    std::vector<glm::vec3>& normals,
+    std::vector<unsigned int>& indices = VEC_UINT_DEFAUTL_VALUE
+);
+
+void indexVBO(
+    const std::vector<glm::vec3>& in_vertices,
+    const std::vector<glm::vec2>& in_uvs,
+    const std::vector<glm::vec3>& in_normals,
+    std::vector<unsigned int> & out_indices,
+    std::vector<glm::vec3> & out_vertices,
+    std::vector<glm::vec2> & out_uvs,
+    std::vector<glm::vec3> & out_normals
+);
 
 class Mesh {
 private:
-    std::vector<GLfloat> vertices;
-    std::vector<GLuint> faces;
-    std::vector<GLfloat> colors;
+    std::vector<glm::vec3> vertices, normals, indexedVertices, indexedNormals;
+    std::vector<glm::vec2> uvs, indexedUVS;
+    std::vector<unsigned int> indices;
 
-    GLuint VAO;
-    GLuint vertexVBO;
-    GLuint colorVBO;
-    GLuint EBO;
-    
+    GLuint VAO, verticesVBO, uvsVBO, normalsVBO, EBO;
+
     glm::vec3 position = glm::vec3(0.0f);
     glm::vec3 scale = glm::vec3(1.0f);
 
 public:
-    Mesh();
+    Mesh(
+        const std::vector<glm::vec3>& vertices,
+        const std::vector<glm::vec2>& uvs,
+        const std::vector<glm::vec3>& normals
+    );
     Mesh(std::string filename);
     ~Mesh() {unloadVram();}
 
