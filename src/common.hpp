424586e0e328381688623da233ea5d5a8e0f644e
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-12-26 12:39:36 +0200

    begin implementing scene graph with leaf, node, transformation

--- src/common.hpp
+++ src/common.hpp
@@ -1,8 +1,14 @@
 #ifndef COMMON_HPP
 #define COMMON_HPP
 
+#include <vector>
+#include <memory>
+#include <GL/glew.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "shader.hpp"
 
 #define DEFAULT_POSITION glm::vec3(0)
 #define DEFAULT_ORIENTATION glm::vec3(0)
@@ -47,5 +53,28 @@ public:
 
     void lookAt(glm::vec3 target, glm::vec3 up = glm::vec3(0, 1, 0), glm::vec3 alternativeUp = glm::vec3(0,0,1));
 };
+
+struct Leaf {
+    virtual void draw(glm::mat4 modelMatrix, Shader& shader) const = 0;
+};
+
+struct Node : Leaf {
+    std::vector<std::shared_ptr<Node>> children;
+
+    virtual void draw(glm::mat4 modelMatrix, Shader& shader) const {
+        for (auto& child : children) {
+            child->draw(modelMatrix, shader);
+        }
+    }
+};
+
+struct Transformation : private Orientable, public Node {
+    void draw(glm::mat4 modelMatrix, Shader& shader) const override {
+        modelMatrix = modelMatrix * this->modelMatrix();
+
+        Node::draw(modelMatrix, shader);
+    }
+};
+
 
 #endif
