b6aa0f1cbaee17423d65f8a0b75d4340de06e667
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2024-09-12 16:33:42 +0300

    remove functions from mesh public interface

--- src/mesh.hpp
+++ src/mesh.hpp
@@ -37,24 +37,6 @@ struct Vertex {
     };
 };
 
-void loadOBJWithTiny(
-    const std::string& path,
-    std::vector<glm::vec3>& vertices,
-    std::vector<glm::vec2>& uvs,
-    std::vector<glm::vec3>& normals,
-    std::vector<unsigned int>& indices = VEC_UINT_DEFAUTL_VALUE
-);
-
-void indexVBO(
-    const std::vector<glm::vec3>& in_vertices,
-    const std::vector<glm::vec2>& in_uvs,
-    const std::vector<glm::vec3>& in_normals,
-    std::vector<unsigned int> & out_indices,
-    std::vector<glm::vec3> & out_vertices,
-    std::vector<glm::vec2> & out_uvs,
-    std::vector<glm::vec3> & out_normals
-);
-
 struct AABB {
     glm::vec3 min;
     glm::vec3 max;
